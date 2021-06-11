d9ee5861e0de77cc9162766e9aee0eaf59e9fa46
Indika Wijesooriya <indikman@gmail.com>
2021-06-11 20:40:39 +0530

    Final lecture

---
+++ Assets/Scripts/HorrorTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorrorTest : MonoBehaviour
+{
+
+    public AudioClip horrorSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "gameover")
+        {
+            other.gameObject.GetComponent<AudioSource>().PlayOneShot(horrorSound);
+        }
+    }
+}
